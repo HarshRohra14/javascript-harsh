8aa73a143623ab3b577d565042b1aa212415cc42
Harsh Rohra <rohrah31@gmail.com>
2025-05-24 19:52:11 +0000

    hh

---
+++ 01_basics/dt.js
@@ -0,0 +1,14 @@
+let score = "44abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedin = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
