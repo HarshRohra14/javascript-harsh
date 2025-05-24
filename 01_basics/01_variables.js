a1fc3bf733f6bea14dc25064bf315f529fd3a285
Harsh Rohra <rohrah31@gmail.com>
2025-05-24 19:21:29 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accid = 142
+let accemail = "harsh@.com"
+var accpass = "676767"
+acccity = "UNR"
+
+//accid = 2 // not allowed
+
+accemail = "goliya"
+accpass = "717171"
+acccity = "THANE"
+
+console.log(accid);
+console.table([accid,accemail,accpass,acccity]);
+
+//prefer not to use var cuz of block scope and func scope //
